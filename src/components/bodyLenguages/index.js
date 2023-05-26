3409de91677a4bc93719f69d30c92987678e4d12
navilla92941 <navilla92941@umanizales.edu.co>
2023-05-26 10:19:45 -0500

    7nick

--- src/components/bodyLenguages/index.js
+++ src/components/bodyLenguages/index.js
@@ -1,46 +1,45 @@
 import React from "react";
 
-function Languages(props){
-    return(
-      <div className='information--languages'>
-         <hr className='linea--hr1'/>
-        <h4>{props.languages}</h4>
-        <p>{props.Spanish}</p>
-        <p>{props.English}</p>
-   
-
-      </div>
-    );
-  }
-
-const myInfoLanguages={
-    languages:'Idiomas',
-    English:'Español : Avanzado'
-  }
-
-function CurriculumLenguages(props){
-    return(
-      <div>
-        <Languages
-          languages={props.languages}
-          Spanish={props.Spanish}
-          English={props.English}
-        />
-      </div>
-    );
-  }
-  
-function OfMainLenguages(){
-    return(
-        <div>
-            <CurriculumLenguages
-            languages={myInfoLanguages.languages}
-            Spanish={myInfoLanguages.Spanish}
-            English={myInfoLanguages.English}
-            />
-        </div>
-    );
+function Languages(props) {
+  return (
+    <div className='information--languages' style={{ textAlign: 'center' }}>
+      <hr className='linea--hr1' />
+      <h4>{props.languages}</h4>
+      <p>{props.Spanish}</p>
+      <p>{props.English}</p>
+    </div>
+  );
 }
 
-export default OfMainLenguages;
+const myInfoLanguages = {
+  languages: 'Idiomas',
+  English: 'Español: Avanzado'
+};
+
+function CurriculumLanguages(props) {
+  return (
+    <div>
+      <Languages
+        languages={props.languages}
+        Spanish={props.Spanish}
+        English={props.English}
+      />
+    </div>
+  );
+}
+
+function OfMainLanguages() {
+  return (
+    <div>
+      <CurriculumLanguages
+        languages={myInfoLanguages.languages}
+        Spanish={myInfoLanguages.Spanish}
+        English={myInfoLanguages.English}
+      />
+    </div>
+  );
+}
+
+export default OfMainLanguages;
+
   
