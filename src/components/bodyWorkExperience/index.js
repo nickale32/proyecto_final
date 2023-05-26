bbaff0efcbd0133d045b978cb37cfb58966fe2ad
navilla92941 <navilla92941@umanizales.edu.co>
2023-05-26 10:17:40 -0500

    6nick

--- src/components/bodyWorkExperience/index.js
+++ src/components/bodyWorkExperience/index.js
@@ -1,40 +1,40 @@
 import React from "react";
 
-function WorkExperience(props){
-    return(
-      <div className='information--workExperince'>
-        <h4>{props.workExperience}</h4>
-        <hr className='linea--hr1'/>
-        <p>{props.text1}</p>
-      </div>
-    );
-  }  
-
-const myInfoWorkExperience={
-    workExperience:'Experiencia Laboral',
-    text1:'Accesor comercial'
-  }
-
-function CurriculumWorkExperience(props){
-    return(
-        <div>
-        <WorkExperience
-        workExperience={props.workExperience}
-        text1={props.text1}
-        />
-        </div>
-    );
+function WorkExperience(props) {
+  return (
+    <div className='information--workExperince' style={{ textAlign: 'center' }}>
+      <h4>{props.workExperience}</h4>
+      <hr className='linea--hr1' />
+      <p>{props.text1}</p>
+    </div>
+  );
 }
 
-function OfMainWorkExperience(){
-    return(
-        <div>
-            <CurriculumWorkExperience
-            workExperience={myInfoWorkExperience.workExperience}
-            text1={myInfoWorkExperience.text1}
-            />
-        </div>
-    );
+const myInfoWorkExperience = {
+  workExperience: 'Experiencia Laboral',
+  text1: 'Accesor comercial'
+};
+
+function CurriculumWorkExperience(props) {
+  return (
+    <div>
+      <WorkExperience
+        workExperience={props.workExperience}
+        text1={props.text1}
+      />
+    </div>
+  );
+}
+
+function OfMainWorkExperience() {
+  return (
+    <div>
+      <CurriculumWorkExperience
+        workExperience={myInfoWorkExperience.workExperience}
+        text1={myInfoWorkExperience.text1}
+      />
+    </div>
+  );
 }
 
 export default OfMainWorkExperience;
